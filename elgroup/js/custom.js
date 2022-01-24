f5cf9f94ecdbe80a7fc8464ac9d37365869407f8
yuziwoo <yuziwoo@naver.com>
2022-01-24 14:24:04 +0900

    elgroup - header 작업 중

---
+++ elgroup/js/custom.js
@@ -0,0 +1,5 @@
+window.addEventListener("DOMContentLoaded", (e) => {
+  let header_gnb_list = document.getElementsByClassName("header_gnb_list");
+  let header_list_hover = document.getElementsByClassName("header_list_hover");
+
+});
